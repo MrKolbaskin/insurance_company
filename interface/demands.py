486e2a5dff337ec3cb4a62773aa45067efad07f1
Vladimir Dubinin <vladimir.dubinin@bostongene.com>
2021-03-29 13:40:37 +0300

    Updated interface

--- interface/demands.py
+++ interface/demands.py
@@ -1,16 +1,47 @@
 import PySimpleGUI as sg
 
+SIZE_FONT = ('default', 12)
+INPUT_FONT = ('default', 12)
+
+INPUT_SIZE = (15, None)
+VAL_MES = 'Только целое число'
+
+IS_VISIBLE = False
+INPUT_EVENTS = True
+
 def create_demand_form(name):
     return sg.Column([
-    [sg.Text(name, font=('default', 12),  justification='left')],
-    [sg.Text('Максимальная сумма выплаты', size=(20, None)),
-    sg.InputText(size=(10, None), key=f'{name}.max_sum')],
-    [sg.Text('Продолжительность контракта', size=(20, None)),
-    sg.InputText(size=(10, None), key=f'{name}.duration')],
-    [sg.Text('Сумма взноса', size=(20, None)),
-    sg.InputText(size=(10, None), key=f'{name}.contribution_value')],
-    [sg.Text('Максимальное кол-во страховых случаев в месяц', size=(20, None)),
-    sg.InputText(size=(10, None), key=f'{name}.max_events')]
+    [sg.Text(name, font=('default', 15),  justification='left')],
+    [sg.Text('Максимальная сумма выплаты', size=(20, None), font=SIZE_FONT),
+    sg.Column([
+        [sg.InputText(size=INPUT_SIZE, key=f'{name}.max_sum', default_text='10000', font=INPUT_FONT, enable_events=INPUT_EVENTS)],
+        [sg.Text(VAL_MES, size=INPUT_SIZE, font=INPUT_FONT, visible=IS_VISIBLE, background_color='red', key=f'{name}.max_sum.num')],
+        [sg.Text('Значение >= 1', size=INPUT_SIZE, font=INPUT_FONT, visible=IS_VISIBLE, background_color='red', key=f'{name}.max_sum.seq')]])
+    ],
+    [sg.Text('Продолжительность контракта', size=(20, None), font=SIZE_FONT),
+    sg.Column([
+        [sg.InputText(size=INPUT_SIZE, key=f'{name}.duration', default_text='12', font=INPUT_FONT, enable_events=INPUT_EVENTS)],
+        [sg.Text(VAL_MES, size=INPUT_SIZE, font=INPUT_FONT, visible=IS_VISIBLE, background_color='red', key=f'{name}.duration.num')],
+        [sg.Text('Значение >= 1', size=INPUT_SIZE, font=INPUT_FONT, visible=IS_VISIBLE, background_color='red', key=f'{name}.duration.seq')]])
+    ],
+    [sg.Text('Сумма взноса', size=(20, None), font=SIZE_FONT),
+    sg.Column([
+        [sg.InputText(size=INPUT_SIZE, key=f'{name}.contribution_value', default_text='5000', font=INPUT_FONT, enable_events=INPUT_EVENTS)],
+        [sg.Text(VAL_MES, size=INPUT_SIZE, font=INPUT_FONT, visible=IS_VISIBLE, background_color='red', key=f'{name}.contribution_value.num')],
+        [sg.Text('Значение >= 1', size=INPUT_SIZE, font=INPUT_FONT, visible=IS_VISIBLE, background_color='red', key=f'{name}.contribution_value.seq')]])
+    ],
+    [sg.Text('Максимальное кол-во страховых случаев в месяц', size=(20, None), font=SIZE_FONT),
+    sg.Column([
+        [sg.InputText(size=INPUT_SIZE, key=f'{name}.max_events', default_text='3', font=INPUT_FONT, enable_events=INPUT_EVENTS)],
+        [sg.Text(VAL_MES, size=INPUT_SIZE, font=INPUT_FONT, visible=IS_VISIBLE, background_color='red', key=f'{name}.max_events.num')],
+        [sg.Text('Значение >= 1', size=INPUT_SIZE, font=INPUT_FONT, visible=IS_VISIBLE, background_color='red', key=f'{name}.max_events.seq')]])
+    ],
+    [sg.Text('Текущий спрос, случайная величина', size=(20, None), font=SIZE_FONT),
+    sg.Column([
+        [sg.InputText(size=INPUT_SIZE, key=f'{name}.random_count', default_text='10', font=INPUT_FONT, enable_events=INPUT_EVENTS)],
+        [sg.Text(VAL_MES, size=INPUT_SIZE, font=INPUT_FONT, visible=IS_VISIBLE, background_color='red', key=f'{name}.random_count.num')],
+        [sg.Text('Значение >= 1', size=INPUT_SIZE, font=INPUT_FONT, visible=IS_VISIBLE, background_color='red', key=f'{name}.random_count.seq')]])
+    ]
     ],
     justification='left',
     key=name)
--- interface/state_company.py
+++ interface/state_company.py
@@ -1,6 +1,17 @@
-def state_company():
-    return """Страховая компания
-    Капитал в у.е. : 1000
-    Кол-во контрактов: 10
-    Кол-во страховых случаев за месяц: 3
+def state_company(company, cur_month, current_demand):
+    return f"""Текущий месяц: {cur_month}
+Страховая компания
+    Капитал в у.е. : {company.capital}
+    Кол-во договоров страхование автомобиля: {len(company.contracts['AutoContract'])}
+    Кол-во договоров страхование жизни: {len(company.contracts['LifeContract'])}
+    Кол-во договоров страхование имущества: {len(company.contracts['HouseContract'])}
+"""
+
+def current_demands(current_demand):
+    return f"""
+
+
+Коэффициент текущего спроса: {current_demand['AutoContract']}
+Коэффициент текущего спроса: {current_demand['LifeContract']}
+Коэффициент текущего спроса: {current_demand['HouseContract']}
 """
